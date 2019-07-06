207fb2a75099566ea06159afa613e38e0af31695
Joseph Mancuso <idmann509@gmail.com>
2019-07-06 13:02:15 -0400

    updated for Masonite API

--- trigger_build.py
+++ trigger_build.py
@@ -1,10 +1,43 @@
+"""
+This is a script to easily execute circle CI jobs from other repository builds. 
+Useful if you have a dependent build that you need to run to ensure the latest code is up to date with the parent build.
+
+This script will:
+
+  - Fire a build in another circle CI repository
+  - Loop and call the build_url to continuously get the status
+  - Wait until the job finishes
+  - If the job fails this script will exit 1 which will fail the job. 
+  - Else it will exit 0 which will pass the job.
+  
+**You will need to download this script and put it in the base of your repository**
+
+Usage:
+    - python trigger_build.py --repo user/repo --branch branch_name --token CIRCLE_TOKEN --build ENV_VARIABLE=value --build ENV_VARIABLE2=value2
+    
+Example:
+    - python trigger_build.py --repo masoniteframework/validation --branch circle-ci --token $CIRCLE_TOKEN --build MASONITE_BRANCH=$CIRCLE_BRANCH
+
+NOTE: You can get your CircleCI access token via your dashboard. If no token is specified it will use the CIRCLE_TOKEN in your job dashboard.
+So if you don't want to pass in the token then put it in the environment variable in your job's dashboard.
+
+Example config:
+version: 2
+jobs:
+    Masonite Validation:
+      docker:
+        - image: circleci/python:3.6
+      steps:
+        - checkout
+        - run: python trigger_build.py --repo masoniteframework/validation --branch circle-ci --build MASONITE_DEPENDENT_BRANCH=$CIRCLE_BRANCH
+"""
+
 import requests
 import time
 import os
 import argparse
 parser = argparse.ArgumentParser()
 
-#-db DATABSE -u USERNAME -p PASSWORD -size 20
 parser.add_argument("-r", "--repo", help="Repository name")
 parser.add_argument("-b", "--branch", help="Branch name")
 parser.add_argument("-t", "--token", help="Circle CI Token")
@@ -19,7 +52,7 @@ parameters = {}
 for argument in args.build or []:
     if not '=' in argument:
         print("ERROR: '--build' argument must contain a '=' sign. Got '{}'".format(argument))
-        exit()
+        exit(1)
     key = argument.split('=')[0]
     value = argument.split('=')[1]
 
@@ -32,7 +65,7 @@ r = requests.post('https://circleci.com/api/v1/project/{}/tree/{}?circle-token={
 if 'build_num' not in r.json():
     print('ERROR: Could not find repository {} or with the branch {}'.format(repo, branch))
     print(r.json())
-    exit()
+    exit(1)
 
 print('Building: ', r.json()['build_num'])
 
