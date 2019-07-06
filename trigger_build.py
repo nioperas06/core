82fc956d036d2241e19da746e8d051de57220559
Joseph Mancuso <idmann509@gmail.com>
2019-07-06 08:41:57 -0400

    added build parameter

--- trigger_build.py
+++ trigger_build.py
@@ -8,26 +8,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--repo", help="Repository name")
 parser.add_argument("-b", "--branch", help="Branch name")
 parser.add_argument("-t", "--token", help="Circle CI Token")
-
+parser.add_argument('-a', '--build', action='append', help='Set Build Arguments')
 args = parser.parse_args()
 
 repo = args.repo
 branch = args.branch or 'master'
 token = args.token or os.getenv('CIRCLE_TOKEN')
-r = requests.post('https://circleci.com/api/v1/project/{}/tree/{}?circle-token={}'.format(repo, branch, token))
 
-if 'build_num' not in r.json():
-    print('ERROR: Could not find repository {} or with the branch {}'.format(repo, branch))
+parameters = {}
+for argument in args.build or []:
+    if not '=' in argument:
+        print("ERROR: '--build' argument must contain a '=' sign. Got '{}'".format(argument))
+        exit()
+    key = argument.split('=')[0]
+    value = argument.split('=')[1]
 
-print('Building: ', r.json()['build_num'])
+    parameters.update({key: value})
 
-status = requests.get('https://circleci.com/api/v1.1/project/github/{}/{}?circle-token={}'.format(repo, r.json()['build_num'], token))
-print('Build Status: ', status.json()['lifecycle'])
-while status.json()['lifecycle'] != 'finished':
-    time.sleep(3)
-    print('Build Status: ', status.json()['lifecycle'])
-    status = requests.get('https://circleci.com/api/v1.1/project/github/{}/{}?circle-token={}'.format(repo, r.json()['build_num'], token))
+build_parameters = {'build_parameters': parameters}
+print(build_parameters)
+exit
 
-print('Finished. Failed? ', status.json()['failed'])
-if status.json()['failed']:
-    exit(1)
+# r = requests.post('https://circleci.com/api/v1/project/{}/tree/{}?circle-token={}'.format(repo, branch, token))
+
+# if 'build_num' not in r.json():
+#     print('ERROR: Could not find repository {} or with the branch {}'.format(repo, branch))
+
+# print('Building: ', r.json()['build_num'])
+
+# status = requests.get('https://circleci.com/api/v1.1/project/github/{}/{}?circle-token={}'.format(repo, r.json()['build_num'], token))
+# print('Build Status: ', status.json()['lifecycle'])
+# while status.json()['lifecycle'] != 'finished':
+#     time.sleep(3)
+#     print('Build Status: ', status.json()['lifecycle'])
+#     status = requests.get('https://circleci.com/api/v1.1/project/github/{}/{}?circle-token={}'.format(repo, r.json()['build_num'], token))
+
+# print('Finished. Failed? ', status.json()['failed'])
+# if status.json()['failed']:
+#     exit(1)
